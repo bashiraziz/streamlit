9e43e0091e9e8618eeebff4bca7a0f8f2b372407
Bashir Aziz <bashiraziz@users.noreply.github.com>
2023-11-28 02:11:22 +0000

    Changes to home page

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.title("Provided by of Bashir Aziz")
 st.write(f"All three pages will provided similar response
          1. Compute Percent Complete page does not make a call to OpenAI API
          2. Ask me to Compute Percent Complete, makes a call to OpenAI API and uses a custum function to compute the percent complete
-         3. Ask me a Question page makes a call to OpenAI API to answer a question")
+         3. Ask me a Question page makes a call to OpenAI API to answer a question and provides a response received from OpenAI API")
