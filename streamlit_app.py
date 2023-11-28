09b1ca91c38318d36d24456f269ff8d1009a4dc8
Bashir Aziz <bashiraziz@users.noreply.github.com>
2023-11-28 02:19:50 +0000

    markdown added

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit as st
 from openai import OpenAI
 st.title("Provided by of Bashir Aziz")
 
-st.write(f"All three pages will provided similar response
+note= ("""All three pages will provided similar response 
          1. Compute Percent Complete page does not make a call to OpenAI API
          2. Ask me to Compute Percent Complete, makes a call to OpenAI API and uses a custum function to compute the percent complete
-         3. Ask me a Question page makes a call to OpenAI API to answer a question and provides a response received from OpenAI API")
+         3. Ask me a Question page makes a call to OpenAI API to answer a question and provides a response received from OpenAI API""")
+
+st.markdown(note)
