a2ad3c5918680b861d1e14dfa75541ac1e608bcd
Bashir Aziz <bashiraziz@users.noreply.github.com>
2023-11-29 21:38:30 +0000

    modified note

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from openai import OpenAI
 st.title("Provided by of Bashir Aziz")
 
-note= (''' All three pages will provided similar response \n 1. "Ask me a Question" page makes a call to OpenAI API to get an answer to a question and provides the response received from OpenAI. \n 2. "Ask me to Compute Percent Complete" page makes a call to OpenAI API and OpenAI uses a "custom in-house built" function sent with the call to compute the percent complete. \n3. "Compute Percent Complete" page does not make a call to OpenAI API and uses a custom in-house function to compute percent complete. \n''')
+note= (''' All three pages will provided similar response \n 1. "Ask me a Question" page makes a call to OpenAI API to get an answer to a question and provides the response received from OpenAI. \n 2. "Ask me to Compute Percent Complete" page makes a call to OpenAI API and OpenAI uses a "custom in-house built" function sent with the call to compute the percent complete.** \n3. "Compute Percent Complete" page does not make a call to OpenAI API and uses a custom in-house function to compute percent complete. \n''')
 function_calling = ('''**Learn more about function calling by accessing the link below:\n https://platform.openai.com/docs/guides/function-calling''')
 blank_lines = ('''\n\n''')
 st.markdown(note)
